b406041c3c56948b327aece4cccd00d6073a76da
Samuel Martin <samumartinfrias@gmail.com>
2026-02-18 16:20:14 +0000

    following tutorial to min 8:00

--- packages/data-ops/src/database/setup.ts
+++ packages/data-ops/src/database/setup.ts
@@ -1,5 +1,4 @@
-// packages/data-ops/src/database/setup.ts
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 let db: ReturnType<typeof drizzle>;
 
@@ -11,7 +10,8 @@ export function initDatabase(connection: {
   if (db) {
     return db;
   }
-  db = drizzle({ connection });
+  const connectionString = `postgres://${connection.username}:${connection.password}@${connection.host}`;
+  db = drizzle(connectionString);
   return db;
 }
 
